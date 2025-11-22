9c0b897bc67280662ccf33897067307ab5651a20
Gabriel Cardoso de Faria <gacarfaria@gmail.com>
2025-11-22 13:58:54 -0300

    chore: remover código obsoleto (src/rag, src/peripheral) não usado na arquitetura simplificada

--- src/peripheral/__init__.py
+++
@@ -1,9 +0,0 @@
-"""
-Sistema Nervoso Periférico (SNP) - Corpo da IA
-Conecta a IA (cérebro) com o sistema Linux (corpo)
-"""
-
-from src.peripheral.interface import PeripheralInterface
-
-__all__ = ['PeripheralInterface']
-
